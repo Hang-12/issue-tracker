913a8d0ad50c5fcff6e80830478dc49000aa22b8
Hang <sahzonghang@gmail.com>
2024-04-08 23:41:47 +0800

    Update and sort import

---
+++ app/components/index.ts
@@ -0,0 +1,12 @@
+// import Link from "next/link";
+// import ErrorMessage from "./ErrorMessage";
+// import IssueStatusBadge from "./IssueStatusBadge";
+// import Spinner from "./Spinner";
+
+// export { Link, ErrorMessage, IssueStatusBadge, Spinner };
+
+export { default as Link } from "./Link";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as IssueStatusBadge } from "./IssueStatusBadge";
+export { default as Spinner } from "./Spinner";
+export { default as Skeleton } from "./Skeleton";
